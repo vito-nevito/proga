ca8b95e22fa87fc0b5323f86234626480e3d0901
vito-nevito <130160691+vito-nevito@users.noreply.github.com>
2023-09-20 22:19:29 +0300

    task_1

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-using namespace std;
 
 int main(){
-double **p = 0;
-p = new double*(new double(2));
-cout << **p << endl;
-delete *p;
-delete p;
+double **p1 = 0;
+p1 = new double*(new double);
+**p1 = 2;
+std::cout << **p1 << std::endl;
+delete *p1;
+delete p1;
+
+double **p2 = 0;
+p2 = new double*(new double(2));
+std::cout << **p2 << std::endl;
+delete *p2;
+delete p2;
+
+int N = 10;
+auto c1 = new int[N][3];
+double* c2 = new double[]{1, 2, 3};
 }
