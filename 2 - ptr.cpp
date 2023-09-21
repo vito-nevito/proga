a8e6b2398df4a7d9165789abf3e379c02f124f79
vito-nevito <130160691+vito-nevito@users.noreply.github.com>
2023-09-21 22:19:32 +0300

    task_1

--- 2 - ptr.cpp
+++ 2 - ptr.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 
 int main(){
+// так можно везде
 double **p1 = 0;
 p1 = new double*(new double);
 **p1 = 2;
 std::cout << **p1 << std::endl;
 delete *p1;
 delete p1;
-
+// так можно везде, сразу инициализируя
 double **p2 = 0;
 p2 = new double*(new double(2));
 std::cout << **p2 << std::endl;
 delete *p2;
 delete p2;
-
+// обьявление динамического массива
+double* c1 = new double[3];
+// обьявление многомероного массива
 int N = 10;
-auto c1 = new int[N][3];
-double* c2 = new double[]{1, 2, 3};
+auto c2 = new int[N][3];
+//auto c2 = new int[3][N];// так нельзя
+//double* c3 = new double[]{1, 2, 3};// сразу ининциализировать начиная с С++20
 }
