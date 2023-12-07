9d468d4374fd666ab9dfac9da6ba9b31d986d911
vito-nevito <130160691+vito-nevito@users.noreply.github.com>
2023-12-07 23:38:22 +0300

    07.12

--- 16-tree/main.cpp
+++ 16-tree/main.cpp
@@ -5,34 +5,76 @@ class CBinTree
 {
     class node
     {
-        T val;
+    public:
+        T key;
         node* left;
         node* right;
     };
 
 protected:
-    node root;
-    T key;
+    node* root;
+    T key_m;
 public:
 
-
-    void insert(T a)
+    CBinTree()
     {
-        p = (node*) malloc(sizeof(node));
-        p->val = a;
-        p->left = NULL;
-        p->right = NULL;
+        root = NULL;
     };
 
-    void delete(T a)
+    CBinTree(std::initializer_list<T> a)
+    {
+        root = NULL;
+        for(T i : a)
+        {
+            (*this).insert_(i);
+        }
+    };
+
+    void insert_(T a)
+    {
+        if(root != NULL)
+        {
+            node* p = (node*) malloc(sizeof(node));
+            p->key = a;
+            p->left = NULL;
+            p->right = NULL;
+            node* t = root;
+            while (t != NULL)
+            {
+                node* tp = t;
+                if (p->key < t->key)
+                {
+                    t = t->left;
+                    if (t==NULL)
+                        tp->left = p;
+                }
+                else
+                {
+                    t = t->right;
+                    if (t==NULL)
+                        tp->right = p;
+                }
+            }
+        }
+        else
+        {
+            root = (node*) malloc(sizeof(node));
+            root->key = a;
+            root->left = NULL;
+            root->right = NULL;
+        }
+    };
+
+    void delete_(T a)
     {
 
     };
 
-    ~CBinTree();
+    ~CBinTree() {};
 };
 
 int main()
 {
-
+    CBinTree<int> S{1, 2, 3};
+    S.insert_(3);
 }
